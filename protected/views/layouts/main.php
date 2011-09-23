e04629c7965a32a13c0f0e8afa33d197fc8a15f2
l2qnew@gmail.com <l2qnew@gmail.com@23be741a-69bd-7fdd-d57a-ad731b93d96a>
2011-09-23 17:08:44 +0000

    Create admin & music module with link in menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,8 +29,11 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
+                                array('label'=>'Music', 'url'=>array('/music/default/index')),
+                                array('label'=>'Admin', 'url'=>array('/admin/default/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+                                array('label'=>'Gii', 'url'=>array('/gii')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
