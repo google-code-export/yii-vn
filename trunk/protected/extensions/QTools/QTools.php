08b6501811741585ff53b685e07ff7dee51c94da
l2qnew@gmail.com <l2qnew@gmail.com@23be741a-69bd-7fdd-d57a-ad731b93d96a>
2011-10-13 14:44:57 +0000

    new db & controller

--- trunk/protected/extensions/QTools/QTools.php
+++ trunk/protected/extensions/QTools/QTools.php
@@ -16,23 +16,25 @@ class QTools extends CApplicationComponent {
      * return array if success else return false
      */
     public static function getAllNameControllerOfModule($moduleName = null) {
-        $listController   = array();
-        $pathControllers  = Yii::app()->getBasePath().'\controllers';
-        if(null != $moduleName) {
-            $pathControllers = Yii::app()->getModule($moduleName)->getBasePath().'\controllers';
+        $listController = array();
+        $pathControllers = Yii::app()->getBasePath() . '\controllers';
+        if (null != $moduleName) {
+            $pathControllers = Yii::app()->getModule($moduleName)->getBasePath() . '\controllers';
         }
-        $setPathFiles       = Yii::app()->file->set($pathControllers, true);
-        $listPathFiles = $setPathFiles->getContents(false,'/Controller.php/');
+        $setPathFiles = Yii::app()->file->set($pathControllers, true);
+        $listPathFiles = $setPathFiles->getContents(false, '/Controller.php/');
         foreach ($listPathFiles as $pathFile) {
             $file = Yii::app()->file->set($pathFile, true);
             $fileName = $file->getFilename();
-            $controllerName = preg_replace('/Controller/','', $fileName);
-            array_push($listController , $controllerName);
+            $controllerName = preg_replace('/Controller/', '', $fileName);
+            array_push($listController, $controllerName);
         }
-        if($listController) 
+        if ($listController)
             return $listController;
-        else return false;
+        else
+            return false;
     }
+
     /**
      * Set menu for all controller of module
      * @param type $moduleName
@@ -41,21 +43,43 @@ class QTools extends CApplicationComponent {
     public static function setMenuControllerOfModule($moduleName = null) {
         $menuControllers = array();
         $listLabel = self::getAllNameControllerOfModule($moduleName);
-        if($listLabel) {
+        if ($listLabel) {
             /* @var $listLabel type */
             foreach ($listLabel as $menu) {
                 $menu = strtolower($menu);
-                if(null==$moduleName) 
-                    $url = '/'.$menu; 
-                else 
-                    $url = '/'.$moduleName.'/'.$menu; 
-                $label = 'Manage '.$menu;
-                $item = array('label'=>$label, 'url'=>array($url));
+                if (null == $moduleName)
+                    $url = '/' . $menu;
+                else
+                    $url = '/' . $moduleName . '/' . $menu;
+                $label = 'Manage ' . $menu;
+                $item = array('label' => $label, 'url' => array($url));
                 array_push($menuControllers, $item);
             }
             return $menuControllers;
         }
-        else return FALSE;
+        else
+            return FALSE;
+    }
+
+    /**
+     * Get image link 
+     * @param type $image 
+     * @param type $folder
+     * @return string 
+     */
+    static function getImageLink($image,$folder) {
+        $img_link = Yii::app()->request->baseUrl .$folder.$image;
+        return $img_link;
+    }
+    /**
+     * Get image thumnail link 
+     * @param type $image
+     * @param type $folder
+     * @return string 
+     */
+    static function getImageThumbnailLink($image,$folder) {
+        $img_link = Yii::app()->request->baseUrl .$folder. $image;
+        return $img_link;
     }
 
 }
